84b6f50a1c62fd65407d7b2123c42d756e849e2b
josegrq <52509499+josegrq@users.noreply.github.com>
2021-03-28 18:19:48 -0600

    Added dependencies to authenticate user and added app router

--- main.js
+++ main.js
@@ -4,24 +4,25 @@ const usersController = require("./controllers/usersController");
 const express = require("express");
 const layouts = require("express-ejs-layouts");
 const mongoose = require("mongoose");
-
+const methodOverride = require("method-override");
 mongoose.connect("mongodb://localhost:27017/Sweat", {
   useNewUrlParser: true,
 });
 
 const app = express();
-
+const router = express.Router();
 app.set("port", process.env.PORT || 3000);
 app.set("view engine", "ejs");
-app.use(layouts);
+router.use(layouts);
 
+/*
 app.get("/", usersController.getWelcomePage);
 app.get("/signup", usersController.getSignUpPage);
-app.get("/login", usersController.getLoginPage);
+app.get("/login", usersController.getLoginPage);*/
 
 //MIDDLEWARE
 //PRE-PROCESSING REQUESTS
-app.use(express.static("public"));
+router.use(express.static("public"));
 
 //We are parsing URL encoded data from the body
 app.use(
@@ -30,14 +31,19 @@ app.use(
   })
 );
 //Interpret body and query string data as JSON
-app.use(express.json());
+router.use(express.json());
+router.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 
 //ROUTES GO HERE
+router.get("/", usersController.getWelcomePage);
+router.get("/signup", usersController.getSignUpPage);
+router.get("/login", usersController.getLoginPage);
 
 //PAGE ERROR HANDLING
-app.use(errorController.pageNotFoundError);
-app.use(errorController.internalServerError);
+router.use(errorController.pageNotFoundError);
+router.use(errorController.internalServerError);
 
+app.use("/", router);
 app.listen(app.get("port"), () => {
   console.log(`Server is running on port: ${app.get("port")}`);
 });
