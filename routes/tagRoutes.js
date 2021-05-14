370984c50d8b736aa2bab84946c903f26ed41ec3
Kyle VanSteelandt <60204098+Green3lement@users.noreply.github.com>
2021-05-13 19:42:51 -0600

    trending will display the tags
    Commit for the deployment

--- routes/tagRoutes.js
+++ routes/tagRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const tagController = require("../controllers/tagController");
 
-router.get("/:id/trending", tagController.index, tagController.indexView);
+router.get("/trending", tagController.index, tagController.indexView);
 
 
 
